a1940830673aebb45ed0a89ba97b49ad4da71d68
howie <howie@2806beff-09c7-43f2-a6b1-43e25f74e3ab>
2009-07-24 21:53:36 +0000

    OO wrapper around autostop files

--- autostop.py
+++ autostop.py
@@ -14,75 +14,70 @@ uid = os.getuid()
 print ''
 
 ###################################################################################
-#
-def showAllRequests(torrentFileList):
-	defaultRatio = None
-	showHashes = False
-	hashes = []
+class Autostopper(object):
+	def __init__(self,autostopDir):
+		self._autostopDir=autostopDir
 
-	if len(torrentFileList) > 0:
-		print 'Showing matching requests for you:'
-		showHashes = True
-		for torrent in torrentFileList:
-			info = infoFromTorrent(torrent)
-			if info == '':
-				continue
-			hash = sha( bencode( info ) ).hexdigest()
-			hashes.append( hash )
-		if len(hashes) == 0:
-			print 'No torrents listed could be found'
-			return
-	else:
-		print 'Showing all requests for you:'
+	def getStopRatioForHash(self, hash,uid):
+		ratio = float(0.0)
+		stopFile = self._getAutostopFile( hash )
 
+		if os.path.exists(stopFile):
+			ratio = ratioFromAutostopFile(stopFile)
+		else:
+			stopFile = self._getAutostopFile( uid )
+			if os.path.exists(stopFile):
+				ratio = ratioFromAutostopFile(stopFile)
+		return ratio
 
-	for root, dir, files in os.walk( AUTOSTOPD_DIR ):
-		for sf in files:
-			stopFile = os.path.join( AUTOSTOPD_DIR, sf )
+	def stopHashAtRatio( self, hash, ratio ):
+		self._createAutostopFile( ratio,hash=hash)
 
-			if not stopFile.endswith( '.xml'):
-				continue
-			if os.stat(stopFile).st_uid != os.getuid():
-				continue
-			stopRatio = ratioFromAutostopFile( stopFile )
+	def stopAllAtRatio( self, ratio ):
+		self._createAutostopFile( ratio )
 
-			if os.path.basename(stopFile) == str(os.getuid()) + '.xml':
-				defaultRatio = stopRatio
-			else:
-				# find torrent with hash?
-				thash = sf.split('.')[0]
-				torrentFile = os.path.join( INCOMING_TORRENT_DIR, (thash + '.torrent') )
-				info = infoFromTorrent( torrentFile )
-				
-				if info != '':
-					if showHashes:
-						if thash in hashes:
-							print '\'%s\': %.2f' % (info['name'], stopRatio)
-					else:
-						print '\'%s\': %.2f' % (info['name'], stopRatio)
-		if defaultRatio:
-			print ''
-			print 'Will stop all torrents at %.2f unless specified otherwise.' % defaultRatio
-###################################################################################
-# 
-def createAutostopFile(autostopFile, hash=None):
-	try:
-		f = open( autostopFile, 'w' )
-		f.write( '<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n' )
-		f.write( '<autostop>\n' )
-		f.write( '\t<stop>\n' )
+	def _getAutostopFile( self, filePart ):
+		return os.path.join( self._autostopDir, filePart + '.xml' )
 
-		if hash:
-			f.write( '\t\t<hash>%s</hash>\n' % hash )
+	def isAutostopping( self, hash ):
+		autostopFile = self._getAutostopFile(hash)
+		return os.path.exists( autostopFile )
 
-		f.write( '\t\t<ratio>%s</ratio>\n' % ratioLevel )
-		f.write( '\t</stop>\n' )
-		f.write( '</autostop>\n' )
-		f.close()
-		os.chmod( autostopFile, 0640 )
-	except:
-		print 'Problem writing out this request!'
-		raise
+	def remove(self, hash):
+		self.deleteAutostopForHash( hash )
+
+	def deleteAutostopForHash( self, hash ):
+		if self.isAutostopping(hash):
+			file = self._getAutostopFile( hash )
+			os.remove( file )
+
+	def _createAutostopFile(self, ratioLevel, hash=None):
+		stopFile = None
+
+		if hash is not None:
+			stopFile = hash
+		else:
+			stopFile = str(os.getuid())
+
+		autostopFile = self._getAutostopFile(stopFile)
+
+		try:
+			f = open( autostopFile, 'w' )
+			f.write( '<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n' )
+			f.write( '<autostop>\n' )
+			f.write( '\t<stop>\n' )	
+
+			if hash:
+				f.write( '\t\t<hash>%s</hash>\n' % hash )
+
+			f.write( '\t\t<ratio>%s</ratio>\n' % ratioLevel )
+			f.write( '\t</stop>\n' )
+			f.write( '</autostop>\n' )
+			f.close()
+			os.chmod( autostopFile, 0640 )
+		except:
+			print 'Problem writing out this request!'
+			raise
 
 ###################################################################################
 #
@@ -91,11 +86,11 @@ def printUsageAndExit():
 	print 'Usage: %s [-d] [-f] <ratio> <file1.torrent> ... <fileN.torrent>' % sys.argv[0]
 	print '-d ==> delete a previous request for this torrent.'
 	print '-f ==> overwrite another request if present.'
-	print 'Optional: --all=<ratio> ==> Set default stop ratio for all torrents'
+	print 'Optional: --all <ratio> ==> Set default stop ratio for all torrents'
 	exit(2)
 
 try:
-	opts,args = getopt.getopt( sys.argv[1:], 'dsf', ['force','delete','show-all','show','all='] )
+	opts,args = getopt.getopt( sys.argv[1:], 'adf', ['force','delete','all'] )
 except getopt.GetoptError:
 	printUsageAndExit()
 
@@ -103,7 +98,7 @@ deleteExisting = False
 ratioLevel = None
 hash = None
 forceOverwrite = False
-showRequests = False
+stopAll = False
 
 # stop on ratio?
 for o,a in opts:
@@ -111,25 +106,13 @@ for o,a in opts:
 		deleteExisting = True
 	if o in ('-f', '--force'):
 		forceOverwrite = True
-	if o in ('-s', '--show-all','--show'):
-		showAllRequests(args)
-		exit()
-	if o == '--all':
-		ratioLevel = 0.0
+	if o in ('-a', '--all'):
+		stopAll = True
 
-		try:
-			ratioLevel = float(a)
-		except:
-			print 'Invalid number for default ratio!'
-			exit(2)
-
-		createAutostopFile( os.path.join( AUTOSTOPD_DIR, str(os.getuid()) ) + '.xml' )
-		print 'Will stop ALL torrents after a ratio of %.2f has been achieved.' % (ratioLevel)
-		print 'If a separate request is made for a torrent, that request overrides the default'
-		exit()
-
-if len(args) == 0 and showRequests == False:
+if len(args) == 0:
 	printUsageAndExit()
+
+stopper = Autostopper(AUTOSTOPD_DIR)
 
 startArg = 1
 
@@ -145,39 +128,40 @@ if not deleteExisting:
 else:
 	startArg = 0
 
-for tfile in args[startArg:]:
-	torrent = infoFromTorrent(tfile)
-	if torrent == '':
-		print 'Cannot find info on torrent %s' % tfile
-		continue
-
-	if not isFileOwnerCurrentUser(tfile):
-		print 'You do not own %s' % tfile
-		continue
-
-	hash = sha( bencode( torrent ) ).hexdigest()
-	autostopFile = os.path.join( AUTOSTOPD_DIR, hash )
-	torrentName = torrent['name']
-
-	autostopFile += '.xml'
-
-	# make a request to autostopd
-	if os.path.exists( autostopFile ) and not forceOverwrite:
-		if deleteExisting:
-			try:
-				os.remove( autostopFile )
-				print 'Request for %s removed!' % tfile
-			except:
-				print 'Problem removing request for torrent %s' % tfile
+if stopAll:
+	stopper.stopAllAtRatio( ratioLevel )
+	print 'Will stop ALL torrents after a ratio of %.2f has been achieved.' % (ratioLevel)
+	print 'If a separate request is made for a torrent, that request overrides the default'
+else:
+	for tfile in args[startArg:]:
+		torrent = infoFromTorrent(tfile)
+		if torrent == '':
+			print 'Cannot find info on torrent %s' % tfile
 			continue
+	
+		if not isFileOwnerCurrentUser(tfile):
+			print 'You do not own %s' % tfile
+			continue
+	
+		hash = sha( bencode( torrent ) ).hexdigest()
+		torrentName = torrent['name']
+	
+		# make a request to autostopd
+		if stopper.isAutostopping(hash) and not forceOverwrite:
+			if deleteExisting:
+				try:
+					stopper.remove(hash)
+					print 'Request for %s removed!' % tfile
+				except:
+					print 'Problem removing request for torrent %s' % tfile
+				continue
+			else:
+				print 'Already have a request for torrent %s, use -d to delete it.' % tfile
+				continue
 		else:
-			print 'Already have a request for torrent %s, use -d to delete it.' % tfile
-			continue
-	else:
-		if deleteExisting:
-			print 'No request for %s found' % torrentName
-			continue
-
-		createAutostopFile(autostopFile, hash)
-
-		print 'Will stop torrent %s at ratio %.2f.' % (torrentName, ratioLevel)
+			if deleteExisting:
+				print 'No request for %s found' % torrentName
+				continue
+	
+			stopper.stopHashAtRatio( hash, ratioLevel )
+			print 'Will stop torrent %s at ratio %.2f.' % (torrentName, ratioLevel)
