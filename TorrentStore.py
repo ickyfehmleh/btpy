5f7c7ea50a099f8edc8e46957ec90db07fe23a0c
howie <howie@2806beff-09c7-43f2-a6b1-43e25f74e3ab>
2010-03-19 23:06:37 +0000

    torrent store and torrent.py merged; torrent.py axed

--- TorrentStore.py
+++ TorrentStore.py
@@ -1,10 +1,70 @@
 #
-# data store for all users
+# torrent store and torrent object
 #
 import os
 import os.path
 import shutil
-from UserDataStore import UserData
+import hashlib
+from BitTornado.bencode import *
+
+class TorrentExistsException( Exception ):
+	pass
+
+class TorrentAlreadyDownloadedException( Exception ):
+	pass
+
+class MismatchedOwnersException( Exception ):
+	pass
+
+class Torrent:
+	def __init__(self,fileName):
+		self.fileName = os.path.abspath(fileName)
+
+		if self.exists():
+			f = open(self.fileName, 'rb')
+			self.metainfo = bdecode(f.read())
+			f.close()
+			self.info = self.metainfo['info']
+
+	def announceURL(self):
+		return self.metainfo['announce']
+
+	def filesInTorrent(self):
+		paths = []
+
+		if self.info != '':
+			for file in self.info['files']:
+				path = ''
+				for item in file['path']:
+					if (path != ''):
+						path = path + "/"
+					path = path + item
+				paths.append( path )
+		return paths
+
+	def hash(self):
+		return hashlib.sha1(bencode(self.info)).hexdigest()
+
+	def exists(self):
+		return os.path.exists( self.fileName )
+
+	def archiveSize(self):
+		pass
+
+	def path(self):
+		return self.fileName
+
+	def torrentName(self):
+		return self.info['name']
+
+	def isFileOwnerCurrentUser(self):
+		return os.stat(self.fileName).st_uid == os.getuid()
+
+	def isActive(self):
+		hash = self.infoHash()
+		fn = os.path.join( INCOMING_TORRENT_DIR, hash + '.torrent' )
+		return os.path.exists( fn )
+
 
 class TorrentStore:
 	__slots__ = ('incomingTorrentDir','fileDelimiter')
@@ -12,6 +72,29 @@ class TorrentStore:
 	def __init__(self,itd):
 		self.incomingTorrentDir=itd
 		self.fileDelimiter=':'
+		self._initDataPath()
+
+	def _initDataPath(self):
+		if not os.path.exists( self.dataDir() ):
+			os.mkdir( self.dataDir() )
+
+		if not self._isDataPathPresent('allowed_trackers.dat'):
+			print 'Creating allowed_trackers.dat!'
+			self._createDataFile( 'allowed_trackers.dat' )
+
+		if not self._isDataPathPresent( 'autostopd' ):
+			print 'Making autostopd dir!'
+			self._createDataDirectory('autostopd')
+	
+	def _isDataPathPresent(self,fn):
+		return os.path.exists( os.path.join( self.dataDir(), fn ) )
+
+	def _createDataFile(self,fileName):
+		f = open( os.path.join( self.dataDir(), fileName ), 'w' )
+		f.close()
+		
+	def _createDataDirectory(self,dirName):
+		os.mkdir( os.path.join( self.dataDir(), dirName ) )
 
 	def torrentXML(self):
 		return os.path.join( self.dataDir(), 'torrents.xml' )
@@ -28,8 +111,8 @@ class TorrentStore:
 	def masterHashList(self):
 		return os.path.join( self.dataDir(),'hashes.dat' )
 
-	def isTorrentActive(self,userData):
-		return self.isTorrentHashActive(userData.hash)
+	def isTorrentActive(self,t):
+		return self.isTorrentHashActive(t.hash())
 
 	def isTorrentHashActive(self,hash):
 		fn = os.path.join( self.incomingTorrentDir, hash + '.torrent' )
@@ -55,25 +138,25 @@ class TorrentStore:
 		f.close();	
 		return rv
 
-	def isTorrentDataPresent(self,userData):
-		return self.isTorrentDataPresentForHash(userData.hash)
+	def isTorrentDataPresent(self,t):
+		return self.isTorrentDataPresentForHash(t.hash())
 
 	def isTorrentDataPresentForHash(self,hash):
 		return os.path.exists( os.path.join( self.incomingTorrentDir, hash ) )
 
-	def isTorrentPaused(self,userData):
-		return self.isTorrentHashPaused(userData.hash)
+	def isTorrentPaused(self,t):
+		return self.isTorrentHashPaused(t.hash())
 
 	def isTorrentHashPaused(self,hash):
 		if self.isTorrentHashActive(hash):
 			return self.isTorrentDataPresent(hash)
 		return False
 
-	def stopTorrent(self,userData):
-		self.stopTorrentHash(userData.hash)
+	def stopTorrent(self,t):
+		self.stopTorrentHash(t.hash())
 
-	def pauseTorrent(self,userData):
-		self.stopTorrent(userData)
+	def pauseTorrent(self,t):
+		self.stopTorrent(t)
 
 	def stopTorrentHash(self,hash):
 		torrentPath = os.path.join( self.incomingTorrentDir, hash + '.torrent' )
@@ -83,11 +166,21 @@ class TorrentStore:
 		except:
 			return False
 
-	def startTorrent(self,torrentPath,hash):
+	def startTorrent(self,t):
+		if not isinstance(t,Torrent):
+			t = Torrent(t)
+		if t.exists():
+			fn = t.path()
+			hash = t.hash()
+			if not self.isTorrentActive(t):
+				self._startTorrentWithHash(fn,hash)
+			else:
+				raise TorrentExistsException,'Already exists'
+
+	def _startTorrentWithHash(self,torrentPath,hash):
 		startPath = os.path.join( self.incomingTorrentDir, hash + '.torrent' )
-		if not self.isTorrentHashActive(hash):
-			shutil.copy( torrentPath, startPath )
-			os.chmod( startPath, 0640 )
+		shutil.copy( torrentPath, startPath )
+		os.chmod( startPath, 0640 )
 
 	def deleteTorrentHashContents(self,hash):
 		if hash is None or hash=='':
