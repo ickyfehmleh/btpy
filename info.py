f09157883e056467da04ae6e3b1094991d958c5a
howie <howie@2806beff-09c7-43f2-a6b1-43e25f74e3ab>
2010-08-06 18:06:17 +0000

    better output

--- info.py
+++ info.py
@@ -10,7 +10,7 @@ import string
 
 # setup args
 try:
-	opts, args = getopt.getopt(sys.argv[1:], 'tnha', ['--tracker', '--name', '--hash', '--all'] )
+	opts, args = getopt.getopt(sys.argv[1:], 'ctnha', ['--contents', '--tracker', '--name', '--hash', '--all'] )
 except getopt.GetoptError:
 	print '%s file1.torrent [--tracker/-t] [--name/-n] [--hash/-h] [--all/-a] fileN.torrent' % sys.argv[0]
 	sys.exit(2)
@@ -18,6 +18,7 @@ except getopt.GetoptError:
 showTracker = False
 showName = False
 showHash = False
+showContents = False
 
 for opt,arg in opts:
 	if opt in ("-t", "--tracker"):
@@ -26,10 +27,13 @@ for opt,arg in opts:
 		showName = True
 	if opt in ("-h", "--hash"):
 		showHash = True
+	if opt in ('-c', '--contents'):
+		showContents=True
 	if opt in ("-a", "--all"):
 		showTracker = True
 		showName = True
 		showHash = True
+		showContents = True
 
 if len(args) == 0:
 	print '%s file1.torrent file2.torrent file3.torrent ...' % argv[0]
@@ -49,8 +53,21 @@ for metainfo_name in args:
 		print '%s is a dead torrent' % metainfo_name
 
 	if showHash:
-		print 'Hash   : %s' % info_hash
+		print 'Hash    : %s' % info_hash
 	if showTracker:
-		print 'Tracker: %s' % metainfo['announce']
+		print 'Tracker : %s' % metainfo['announce']
 	if showName:
-		print 'Name   : %s' % info['name']
+		print 'Name    : %s' % info['name']
+	if showContents:
+		if info.has_key('length'):
+			print 'Contents: %s [%d]' % (info['name'], info['length'])
+		else:
+			print 'Contents: '
+			for file in info['files']:
+				path = ''
+				for item in file['path']:
+					if (path != ''):
+						path = path + "/"
+					path = path + item
+				fsize = human_readable(file['length'])
+				print '        : %s [%s]' % (path, fsize)
